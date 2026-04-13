dc01412d547e7a9ef3109ee97283d2d76c4f68f8
JadenKim-dev <jaden.kim5179@gmail.com>
2026-04-13 19:56:40 +0900

    refactor(playwright-lib): trim index barrel to essential public API

--- packages/playwright-lib/src/index.ts
+++ packages/playwright-lib/src/index.ts
@@ -1,11 +1,4 @@
 export { loadPlatformEnv, type PlatformEnv } from './env.js';
 export { AdminClient, type AdminClientOptions } from './client.js';
-export {
-  testCase,
-  createTestCase,
-  type TestCaseCallback,
-  type PlatformFixtures,
-  type PlaywrightFixtures,
-  type TestCaseDeps,
-} from './test-case.js';
+export { testCase, type PlatformFixtures } from './test-case.js';
 export { StreamingReporter, type StreamingReporterOptions } from './reporter.js';
